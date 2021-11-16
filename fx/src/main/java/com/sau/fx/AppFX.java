c749ef9860353c82a76d6a660c1ca4cd795770c4
Ahmet Özmen <60114994+ozmen54@users.noreply.github.com>
2021-11-16 20:44:18 +0300

    JavaFX tamam.

--- fx/src/main/java/com/sau/fx/AppFX.java
+++ fx/src/main/java/com/sau/fx/AppFX.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class AppFX extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AppFX.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Merhaba");
         stage.setScene(scene);
         stage.show();
     }
